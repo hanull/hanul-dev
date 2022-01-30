6998f377bb3ec2ce555094669f9bb94ce49fa607
이한울 <dkfghg3@naver.com>
2022-01-30 16:00:10 +0900

    chore: 이름 변경, google analytics ID 추가

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: `hanul-dev`,
   description: `Blog posted about back-end, problem-solving, essay`,
-  author: `hanull`,
-  introduction: `새로운 도전과 경험을 즐기는 백엔드 주니어 개발자입니다. `,
+  author: `@이한울(hanull)`,
+  introduction: `새로운 도전과 경험을 즐기는 백엔드 개발자 이한울입니다. `,
   siteUrl: `https://hanul-dev.netlify.app/`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
@@ -27,7 +27,7 @@ module.exports = {
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
   },
-  ga: '0', // Add your google analytics tranking ID
+  ga: 'UA-162506382-1', // Add your google analytics tranking ID
   ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
 
 }
