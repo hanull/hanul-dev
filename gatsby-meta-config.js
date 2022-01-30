dcfcb305c4ffa8ff2510793fb79223c5775744a9
이한울 <dkfghg3@naver.com>
2022-01-30 16:50:45 +0900

    chore: 레이아웃, 깃헙 링크, 코드 블럭 설정 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: `hanul-dev`,
   description: `Blog posted about back-end, problem-solving, essay`,
-  author: `@이한울(hanull)`,
+  author: `이한울(hanull)`,
   introduction: `새로운 도전과 경험을 즐기는 백엔드 개발자 이한울입니다. `,
   siteUrl: `https://hanul-dev.netlify.app/`, // Your blog site url
   social: {
